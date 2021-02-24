d93d2483019e676f24683396618cdaf702773aa5
Mike Carter <elite.mikecarter@gmail.com>
2021-02-23 20:39:33 -0500

    createted data js file

--- employee/src/utils/API.js
+++ employee/src/utils/API.js
@@ -3,6 +3,6 @@ import from Axios from "axios";
 export default {
     // gett all users
     getUsers: function () {
-        return axios.get("https://randomuser.me/api/?results=5000")
+        return axios.get("https://randomuser.me/api/?results=200&nat=us");
     }
 };
