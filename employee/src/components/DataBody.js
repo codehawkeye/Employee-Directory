23e2e2d85743895028dccc5c39ea281b98241c40
Mike Carter <elite.mikecarter@gmail.com>
2021-02-24 09:17:08 -0500

    added code to DataBody

--- employee/src/components/DataBody.js
+++ employee/src/components/DataBody.js
@@ -15,6 +15,33 @@ function DataBody({ users }) {
     return (
         <tbody>
             {users[0] !== undefined && users[0].name !== undefined}
+             user.map(({ login, name, picture, phone, email, }))
+             return ( 
+            <tr key={ligin.uuid}>
+                <td data-th="Image" className="align-middle">
+                    <img
+                        src={picture.medium}
+                        alt={"profile image for " + name.first + name.last}
+                        className="img-responsive"
+                    />
+                </td>
+                <td data-th="Name" className="name-cell align-middle">
+                    {name.first} {name.last}
+                </td>
+                <td data-th="Phone" className="align-middle">
+                    {phone}
+                </td>
+                <td data-th="Email" className="align-middle">
+                    <a href={"mailto:" + email} target="_bla">
+                        {email}
+                    </a>
+                </td>
+                <td data-th="DOB" className="align-middle">
+                    {formatDate(dob.date)}
+                </td>    
+            </tr>
+            
+         );
       </tbody>  
     )
 }
